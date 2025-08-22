4d8240da68db39f28fd538873256d2d52b940c86
jVCss <jvcs.mecatronica@gmail.com>
2025-08-22 15:16:58 -0300

    feat: improve seed to BR

--- config/database.php
+++ config/database.php
@@ -42,6 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
+            'timezone' => env('DB_TZ', '+00:00'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
